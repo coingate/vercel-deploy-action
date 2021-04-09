5bcf2e56a729bdbd0eea29b09315b3dd3ff12aed
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2021-04-09 11:27:53 +0300

    Truncate commit message

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ async function vercelDeploy(deployRef, commitMessage, paramArgs = []) {
     '-m',
     `githubCommitRepo=${context.repo.repo}`,
     '-m',
-    `githubCommitMessage=${commitMessage}`,
+    `githubCommitMessage=${commitMessage.trim().substring(0, 42)}`,
     '-m',
     `githubCommitRef=${deployRef}`,
   ];
